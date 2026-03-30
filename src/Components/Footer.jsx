2c0db45e30ab32ce69d44368fcbc8d6a97817ee9
Siya <107757390+Siya-S10-ai@users.noreply.github.com>
2026-03-30 09:41:04 +0200

    Add footer with author credit
    Update Footer.jsx to render a <footer> element containing a div and a paragraph with the text 'Created by Siyabonga💖.' to display author attribution.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 const Footer = () => {
     return (
         <>
-
+            <footer>
+                <div>
+                    <p>Created by Siyabonga💖.</p>
+                </div>
+            </footer>
         </>
     )
 }
